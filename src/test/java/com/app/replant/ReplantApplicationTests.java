e3667c5a500b718c43353f9789650fa546ea051d
ChatHongPT <suk924600@naver.com>
2026-02-07 16:15:11 +0900

    [feat] - CICD 빌드 테스트

--- src/test/java/com/app/replant/ReplantApplicationTests.java
+++ src/test/java/com/app/replant/ReplantApplicationTests.java
@@ -2,8 +2,21 @@ package com.app.replant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "FRONTEND_URL=http://localhost:8081",
+    "GOOGLE_MAIL=test@test.com",
+    "REPLANT_URL=http://localhost:8080",
+    "firebase.config.path=firebase/replant-admin.json",
+    "app.version.min=0.0.0",
+    "app.version.latest=0.0.0",
+    "app.version.store-url=https://play.google.com/store/apps/details?id=com.anonymous.replantmobileapp",
+    "app.version.message="
+})
 class ReplantApplicationTests {
 
     @Test
